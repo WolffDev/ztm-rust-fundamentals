4c9e5fd671188839386f40907976b0eccffc14c8
David Wolff <dawo@tv2.dk>
2023-07-07 22:47:32 +0200

    removed target

--- billing_app/target/debug/deps/billing_app-045a371b691773a6.d
+++
@@ -1,5 +0,0 @@
-/Users/dawo/dev/priv/learning/rust/ztm/rust_fundamentals/billing_app/target/debug/deps/billing_app-045a371b691773a6.rmeta: src/main.rs
-
-/Users/dawo/dev/priv/learning/rust/ztm/rust_fundamentals/billing_app/target/debug/deps/billing_app-045a371b691773a6.d: src/main.rs
-
-src/main.rs:
--- billing_app/target/debug/deps/billing_app-0c94bc4c188ff310.d
+++
@@ -1,5 +0,0 @@
-/Users/dawo/dev/priv/learning/rust/ztm/rust_fundamentals/billing_app/target/debug/deps/billing_app-0c94bc4c188ff310.rmeta: src/main.rs
-
-/Users/dawo/dev/priv/learning/rust/ztm/rust_fundamentals/billing_app/target/debug/deps/billing_app-0c94bc4c188ff310.d: src/main.rs
-
-src/main.rs:
