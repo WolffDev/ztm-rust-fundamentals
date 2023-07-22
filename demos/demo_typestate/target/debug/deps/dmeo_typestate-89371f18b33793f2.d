9dd6a0b5fe460c8bd2c90adf551016e3cd6535c1
David Wolff <dawo@tv2.dk>
2023-07-22 09:45:29 +0200

    wip

--- demos/demo_typestate/target/debug/deps/dmeo_typestate-89371f18b33793f2.d
+++
@@ -1,5 +0,0 @@
-/Users/dawo/dev/priv/learning/rust/ztm/rust_fundamentals/demos/demo_typestate/target/debug/deps/dmeo_typestate-89371f18b33793f2: src/main.rs
-
-/Users/dawo/dev/priv/learning/rust/ztm/rust_fundamentals/demos/demo_typestate/target/debug/deps/dmeo_typestate-89371f18b33793f2.d: src/main.rs
-
-src/main.rs:
--- demos/demo_typestate/target/debug/dmeo_typestate.d
+++
@@ -1,1 +0,0 @@
-/Users/dawo/dev/priv/learning/rust/ztm/rust_fundamentals/demos/demo_typestate/target/debug/dmeo_typestate: /Users/dawo/dev/priv/learning/rust/ztm/rust_fundamentals/demos/demo_typestate/src/main.rs
